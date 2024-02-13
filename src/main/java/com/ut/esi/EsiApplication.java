9b97ddf02785dd84a9c3a3916dc7eeb9231f20b6
martenka <marten.kahu.001@gmail.com>
2024-02-13 17:13:40 +0200

    feat: empty project

---
+++ src/main/java/com/ut/esi/EsiApplication.java
@@ -0,0 +1,13 @@
+package com.ut.esi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ut/esi/EsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ut.esi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
